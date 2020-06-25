d7f5f9742348da086a534730be501169c8ac6c79
Shannon <the-shadow-of-light@hotmail.com>
2020-06-25 12:58:23 +1000

    finish loading csv to array
    Loaded CVE's to array and sort by attack type

--- cpelookup.go
+++ cpelookup.go
@@ -12,7 +12,8 @@ import (
    "reflect"
 	"pack/downloadmanager"
 	"strings"
-	"time"
+	//"time"
+	"sync"
 )
 
 
@@ -57,9 +58,10 @@ var lstExploits [] obj_file_exploits
 var lstShells [] obj_file_exploits
 var lstCveId [] obj_exploitdb_mapping
 var lstCve[] obj_cve
+var lstAttackTypes[] string
 
 func main() {
-	start := time.Now()
+	//start := time.Now()
 	downloadmanager.StartDownload()
 	//fmt.Println("Download Status: ", dl)
 
@@ -71,14 +73,87 @@ func main() {
 
   loadExploitMapping()
 	loadCsvNvd()
-	t := time.Now()
-	elapsed := t.Sub(start)
-	fmt.Println("Loading Time: ", elapsed)
+	loadAttackTypes()
+	sortAttackType()
+	//compareOldNew()
+	//t := time.Now()
+	//elapsed := t.Sub(start)
+	//fmt.Println("Loading Time: ", elapsed)
+}
+
+func percentageChange(count, total int) float64 {
+	return (float64(count) * float64(100)) / float64(total)
+}
+/*-------------------------------------------------------------------
+sort arrays
+--------------------------------------------------------------------*/
+
+func sortAttackType(){
+	count := 0
+	total := len(lstCve)
+	nocpe := 0
+	for c := range lstCve{
+		if len(lstCve[c].cpe_lst) == 0{
+			count += 1
+			nocpe +=1
+		}else{
+			for at := range lstAttackTypes {
+				if strings.Contains(lstCve[c].description, lstAttackTypes[at]){
+					count += 1
+					break
+				}
+			}
+		}
+	}
+	fmt.Println("Categorized", count, "/" , total, " or ", percentageChange(count,total), "%")
+	//fmt.Println("No Cpe: ", nocpe)
 }
 
 /*-------------------------------------------------------------------
 load arrays
 --------------------------------------------------------------------*/
+
+func loadAttackTypes(){
+	lstAttackTypes = append(lstAttackTypes, "buffer overflow")
+	lstAttackTypes = append(lstAttackTypes, "denial of service")
+	lstAttackTypes = append(lstAttackTypes, "heap overflow")
+	lstAttackTypes = append(lstAttackTypes, "sql injection")
+	lstAttackTypes = append(lstAttackTypes, "remote attackers to execute")
+	lstAttackTypes = append(lstAttackTypes, "directory traversal vulnerability")
+	lstAttackTypes = append(lstAttackTypes, "remote attackers to read")
+	lstAttackTypes = append(lstAttackTypes, "xss")
+	lstAttackTypes = append(lstAttackTypes, "cross-site scripting")
+	lstAttackTypes = append(lstAttackTypes, "cross site scripting")
+	lstAttackTypes = append(lstAttackTypes, "allows remote attackers to inject")
+	lstAttackTypes = append(lstAttackTypes, "allows remote authenticated")
+	lstAttackTypes = append(lstAttackTypes, "allows remote attackers to use a certificate")
+	lstAttackTypes = append(lstAttackTypes, "stack-based buffer overflow")
+	lstAttackTypes = append(lstAttackTypes, "stack based buffer overflow")
+	lstAttackTypes = append(lstAttackTypes, "allows local users to bypass")
+	lstAttackTypes = append(lstAttackTypes, "allows local users to obtain")
+	lstAttackTypes = append(lstAttackTypes, "cross-site request")
+	lstAttackTypes = append(lstAttackTypes, "cross site request")
+	lstAttackTypes = append(lstAttackTypes, "allows local users to expose")
+	lstAttackTypes = append(lstAttackTypes, "allows remote attackers to gain root")
+	lstAttackTypes = append(lstAttackTypes, "allows remote attackers to monitor")
+	lstAttackTypes = append(lstAttackTypes, "allows remote attackers to trick")
+	lstAttackTypes = append(lstAttackTypes, "allows remote attackers to view")
+	lstAttackTypes = append(lstAttackTypes, "heap corruption")
+	lstAttackTypes = append(lstAttackTypes, "allows local users to gain privileges")
+	lstAttackTypes = append(lstAttackTypes, "allows local users to overwrite")
+	lstAttackTypes = append(lstAttackTypes, "allows remote attackers to trigger memory corruption or possibly execute")
+	lstAttackTypes = append(lstAttackTypes, "possibly execute")
+	lstAttackTypes = append(lstAttackTypes, "allow remote attackers to modify")
+	lstAttackTypes = append(lstAttackTypes, "privilege escalation")
+	lstAttackTypes = append(lstAttackTypes, "remote attacker")
+	lstAttackTypes = append(lstAttackTypes, "remote bypass")
+	lstAttackTypes = append(lstAttackTypes, "remote code execution")
+	lstAttackTypes = append(lstAttackTypes, "elevation of privilege")
+
+
+
+}
+
 func loadFileExploits(lst [][]string){
   for i := range lst {
     obj_fe := obj_file_exploits{
@@ -155,7 +230,10 @@ func loadCsvNvd(){
 
 			for d := range csvdata{
 				if d > 0{
-					lstdatasplit := strings.Split(csvdata[d][10],":::")
+					var lstdatasplit [] string
+					if strings.Contains(csvdata[d][10],"cpe"){
+						lstdatasplit = strings.Split(csvdata[d][10],":::")
+					}
 					obj_cveitem := obj_cve{
 						cve: csvdata[d][0],
 				    attack_type: "",
@@ -179,6 +257,30 @@ func loadCsvNvd(){
 		}
 	}
 
+}
+
+func compareOldNew(){
+	var wg sync.WaitGroup
+	old := readCsv("other/CVEYear_tbl.csv")
+	for o := range old{
+		if o > 0{
+			wg.Add(1)
+			go findOld(&wg,old[o])
+		}
+	}
+}
+
+func findOld(wg *sync.WaitGroup, old [] string){
+	defer wg.Done()
+	for c := range lstCve{
+		if strings.ToUpper(old[0]) == lstCve[c].cve{
+			lstCve[c].attack_type = old[1]
+			break
+		}
+	}
+	fmt.Println("Main: Waiting for workers to finish")
+	wg.Wait()
+	fmt.Println("Main: Completed")
 }
 
 /*-------------------------------------------------------------------
